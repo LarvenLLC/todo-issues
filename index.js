c20336f3cd3b3c95a9f0be7b47c47582ff866b63
BossBele <adambeleko@gmail.com>
2021-05-09 11:13:13 +0300

    main

--- index.js
+++ index.js
@@ -2,25 +2,28 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const { promises: fs } = require("fs");
 
-try {
-  // get github token
-  const gitHubToken = core.getInput("repo-token", { required: true });
-  // get TODO path
-  const path = core.getInput("path");
-  const main = async () => {
+const main = async () => {
+  try {
+    // get github token
+    const gitHubToken = core.getInput("repo-token", { required: true });
+    // get TODO path
+    const path = core.getInput("path");
+
     const content = await fs.readFile(path, "utf8");
     core.setOutput("content", content);
-  };
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
-  core.setOutput("issue", response.data.key);
-} catch (error) {
-  core.setFailed(error.message);
-  process.exit(1);
-}
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput("who-to-greet");
+    console.log(`Hello ${nameToGreet}!`);
+    const time = new Date().toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    console.log(`The event payload: ${payload}`);
+    core.setOutput("issue", response.data.key);
+  } catch (error) {
+    core.setFailed(error.message);
+    process.exit(1);
+  }
+};
+
+main().catch((error) => core.setFailed(error.message));
